b7f680ceefea5a88baf3874730486f9533b8db5c
nqmetke <nqmetke@gmail.com>
2016-08-26 21:11:26 -0400

    Added alot

--- app/src/main/java/com/nqmetke/myshs/schedule_main.java
+++ app/src/main/java/com/nqmetke/myshs/schedule_main.java
@@ -9,6 +9,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.annotation.IntegerRes;
 import android.support.design.widget.Snackbar;
@@ -22,6 +24,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -53,6 +56,7 @@ import android.support.design.widget.FloatingActionButton;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 
 public class schedule_main extends AppCompatActivity {
@@ -62,7 +66,6 @@ public class schedule_main extends AppCompatActivity {
     private ArrayList<String>list = new ArrayList<String>();
 
     public static final String PREFS_NAME = "user_courses";
-    private String[] courses = new String[] {};
     List<HashMap<String, String>> fillMaps = new ArrayList<HashMap<String, String>>();
     Calendar c = null;
     Calendar cal = Calendar.getInstance();
@@ -76,6 +79,11 @@ public class schedule_main extends AppCompatActivity {
     int second = 0;
     int real_second = 0;
     int endTimeFinal = 0;
+    List<HashMap<String, String>> prevFill;
+
+
+
+
 
 
 
@@ -84,7 +92,16 @@ public class schedule_main extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_main);
+        System.out.println("Connecting...");
         setTitle("mySHS");
+
+        new JSONTask().execute("http://shstv.herokuapp.com/api/schedule/today");
+        final ListView schedule = (ListView) findViewById(R.id.listView);
+        final String[] col_value = new String[] {"col_number", "col_course", "col_time"};
+        final int[] col_id = new int[] {R.id.item1, R.id.item2, R.id.item3};
+
+
+
         Thread t = new Thread() {
 
             @Override
@@ -95,43 +112,49 @@ public class schedule_main extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                c = Calendar.getInstance();
-                                year = c.get(Calendar.YEAR);
-                                month = c.get(Calendar.MONTH);
-                                day = c.get(Calendar.DAY_OF_MONTH);
-                                hour = c.get(Calendar.HOUR);
-                                minute = c.get(Calendar.MINUTE);
-                                day_name = c.get(Calendar.DAY_OF_WEEK);
-                                second = c.get(Calendar.SECOND);
-                                String val = null;
-                                int am_pm = c.get(Calendar.AM_PM);
-                                 if (am_pm == (Calendar.PM)){
-                                    hour += 12;
+                                ProgressBar progressBar = (ProgressBar) findViewById(R.id.loadingSchedule);
+                                if (fillMaps != prevFill) {
+                                    final SimpleAdapter adapter = new SimpleAdapter(schedule_main.this, fillMaps, R.layout.schedule_layout, col_value, col_id);
+                                    schedule.setAdapter(adapter);
+                                    hideView(progressBar);
                                 }
+                                prevFill = fillMaps;
 
-
-
-                                real_second = hour *60*60 + minute * 60 + second ;
-                                int endPeriod = 0;
-                                for(int i = 0;i<7;i++){
-                                    endPeriod = Integer.parseInt(fillMaps.get(i).get("raw_num"));
-                                    if(real_second < endPeriod){
-                                        break;
+                                    c = Calendar.getInstance();
+                                    year = c.get(Calendar.YEAR);
+                                    month = c.get(Calendar.MONTH);
+                                    day = c.get(Calendar.DAY_OF_MONTH);
+                                    hour = c.get(Calendar.HOUR);
+                                    minute = c.get(Calendar.MINUTE);
+                                    day_name = c.get(Calendar.DAY_OF_WEEK);
+                                    second = c.get(Calendar.SECOND);
+                                    String val = null;
+                                    int am_pm = c.get(Calendar.AM_PM);
+                                    if (am_pm == (Calendar.PM)) {
+                                        hour += 12;
                                     }
 
-                                }
 
-                                TextView time = (TextView)findViewById(R.id.textView2);
-                                int timeLeft = endPeriod - real_second;
-                                int minLeft = timeLeft / 60;
-                                if(minLeft > 0) {
+                                    real_second = hour * 60 * 60 + minute * 60 + second;
+                                    int endPeriod = 0;
+                                    for (int i = 0; i < 6; i++) {
+                                        endPeriod = Integer.parseInt(fillMaps.get(i).get("raw_num"));
+                                        if (real_second < endPeriod) {
+                                            break;
+                                        }
 
-                                    time.setText(Integer.toString(minLeft) + ":" + (60 - second));
-                                }
-                                else {
-                                    String text = "School is over for the day!";
-                                    time.setText(text);
-                                }
+                                    }
+
+                                    TextView time = (TextView) findViewById(R.id.textView2);
+                                    int timeLeft = endPeriod - real_second;
+                                    int minLeft = timeLeft / 60;
+                                    if (minLeft > 0) {
+
+                                        time.setText(Integer.toString(minLeft) + ":" + (60 - second));
+                                    } else {
+                                        String text = "School is over for the day!";
+                                        time.setText(text);
+                                    }
 
                             }
                         });
@@ -144,25 +167,19 @@ public class schedule_main extends AppCompatActivity {
         t.start();
 
 
-        final ListView schedule = (ListView) findViewById(R.id.listView);
-        String[] col_value = new String[] {"col_number", "col_course", "col_time"};
-        int[] col_id = new int[] {R.id.item1, R.id.item2, R.id.item3};
-
-        for (String course : courses) {
-            list.add(course);
-        }
 
 
 
-        final SimpleAdapter adapter = new SimpleAdapter(this, fillMaps ,R.layout.schedule_layout, col_value, col_id);
-        schedule.setAdapter(adapter);
+
+
+
         schedule.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(view.getContext(), course_hw.class);
                 String course_name = (fillMaps.get(position)).get("col_course");
                 String course_num = (fillMaps.get(position)).get("col_number");
-                //String test = "Crap.";
+
                 intent.putExtra("course_name", course_name);
                 intent.putExtra("course_number", course_num);
                 //intent.putExtra("hashMap", fillMaps.get(position).get("col_course"));
@@ -172,7 +189,6 @@ public class schedule_main extends AppCompatActivity {
 
             }
         });
-        new JSONTask().execute("http://shstv.herokuapp.com/api/schedule/today");
 
 
 
@@ -188,17 +204,18 @@ public class schedule_main extends AppCompatActivity {
         protected String doInBackground(String... params) {
             HttpURLConnection connection = null;
             BufferedReader reader = null;
+            System.out.println("Connecting...");
 
 
             try
 
             {
-                //URL url = new URL(params[0]);
-                //connection = (HttpURLConnection) url.openConnection();
-                //connection.connect();
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
 
-                //InputStream stream = connection.getInputStream();
-                InputStream stream = getAssets().open("mainSchedule.json");
+                InputStream stream = connection.getInputStream();
+                //InputStream stream = getAssets().open("mainSchedule.json");
 
                 reader = new BufferedReader(new InputStreamReader(stream));
 
@@ -208,7 +225,8 @@ public class schedule_main extends AppCompatActivity {
                 while ((line = reader.readLine()) != null) {
                     buffer.append(line);
                 }
-
+                System.out.println("Parsing...");
+                System.out.println("Returned: " + buffer.toString());
                 return buffer.toString();
 
             } catch (
@@ -234,14 +252,22 @@ public class schedule_main extends AppCompatActivity {
             return null;
         }
         @Override
-        protected void onPostExecute(String json) {
-            super.onPostExecute(json);
+        protected void onPostExecute(String json_raw) {
+            System.out.println("Starting Post Execute...");
+            super.onPostExecute(json_raw);
             try {
-                SharedPreferences.Editor editor = courses.edit();
-                editor.putString("json", json);
-                editor.commit();
+                SharedPreferences schedules = getSharedPreferences("schedule", 0);
+                SharedPreferences.Editor editor = schedules.edit();
+                editor.putString("json", json_raw);
+                editor.apply();
+                String json = schedules.getString("json", null);
 
-                JSONArray h = new JSONArray(json);
+                System.out.println("Fo Shizzle! This is json: " + json);
+               // System.out.println("Wowie, this is json_raw: " + json_raw);
+
+
+
+                JSONArray jsonArray = new JSONArray(json);
 
 
 
@@ -272,7 +298,7 @@ public class schedule_main extends AppCompatActivity {
                         day_number = 4;
                         break;
                 }
-                JSONArray jsonArray = h.getJSONArray(day_number);
+                //JSONArray jsonArray = h.getJSONArray(day_number);
                 for(int i = 0;i<jsonArray.length();i++){
                     JSONObject e = jsonArray.getJSONObject(i);
                     String course_number = e.getString("name");
@@ -328,13 +354,22 @@ public class schedule_main extends AppCompatActivity {
                             course_name = "";
                             break;
                     }
+                    System.out.println("Done! Adding to Hashmap!");
+                    System.out.println("Course Name: " + course_name);
+                    System.out.println("Course Number: " + e.getString("name"));
+                    System.out.println("Course Time: " + Math.round(startTime) + ((min < 10) ? ":0" : ":") + Math.round(min) + " - " + Math.round(endTime) + ((endMin < 10) ? ":0" : ":") + Math.round(endMin));
+                    System.out.println("Course Name: " + Integer.toString(end_time));
+
 
 
                     HashMap<String, String> map = new HashMap<String, String>();
 
                     map.put("col_time", Math.round(startTime) + ((min < 10) ? ":0" : ":") + Math.round(min) + " - " + Math.round(endTime) + ((endMin < 10) ? ":0" : ":") + Math.round(endMin));
+                    // map.put("col_time", "col_time");
                     map.put("col_course", course_name);
+                    //map.put("col_course", "course_name");
                     map.put("col_number", e.getString("name"));
+                    // map.put("col_number", "col_number");
                     map.put("raw_num", Integer.toString(end_time));
                     fillMaps.add(map);
 
@@ -354,17 +389,15 @@ public class schedule_main extends AppCompatActivity {
 
 
         }
-    }
-
-    void checkPeriod(){
-        for (int i=0; i<fillMaps.size();i++){
-            String val = (fillMaps.get(i)).get("raw_num");
-            System.out.println(val);
-
-        }
 
 
     }
+
+    protected void onStart(){
+        super.onStart();
+        //new JSONTask().execute("http://shstv.herokuapp.com/api/schedule/today");
+    }
+
 
     protected void onStop(){
         super.onStop();
@@ -373,9 +406,9 @@ public class schedule_main extends AppCompatActivity {
 
 
     }
-
-
-
+    public void hideView(View view) {
+        view.setVisibility(View.GONE);
+    }
 }
 
 
